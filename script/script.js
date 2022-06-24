8d9ab636aacc50142a2613bb45b9348be5fbd26b
chrome <86589965+godwin-webdev@users.noreply.github.com>
2022-06-24 16:23:54 +0800

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,14 @@
+// Though javascript is not a requirement for this activity, I have decided to use it
+
+// add class navbarDark on navbar scroll
+const header = document.querySelector('.navbar');
+
+window.onscroll = function() {
+    var top = window.scrollY;
+    if(top >=100) {
+        header.classList.add('navbarDark');
+    }
+    else {
+        header.classList.remove('navbarDark');
+    }
+}
